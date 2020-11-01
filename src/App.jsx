8e34471fe8b2a8ad9f3209e4e26e6ec8c6a1f767
Natch <17216462+Natch@users.noreply.github.com>
2020-11-01 23:49:25 +0900

    jsxの雛形を作成

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,33 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <li>い</li>
+          <button>完了</button>
+          <button>削除</button>
+        </ul>
+        <ul>
+          <li>ろ</li>
+          <button>完了</button>
+          <button>削除</button>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <li>は</li>
+          <button>完了</button>
+          <button>削除</button>
+        </ul>
+      </div>
+    </>
+  );
 };
